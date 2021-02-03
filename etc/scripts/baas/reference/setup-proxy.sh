6bf16506a719447626b176b0fff6216d9b324b0a
João Luzio <joao.luzio@gmail.com>
2021-02-03 21:42:20 +0000

    doc

--- etc/scripts/baas/reference/setup-proxy.sh
+++ etc/scripts/baas/reference/setup-proxy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-export http_proxy=http://webproxy.backbase.com:3128
-export https_proxy=http://webproxy.backbase.com:3128
+export http_proxy=http://webproxy.infra.backbase.cloud:8888
+export https_proxy=http://webproxy.infra.backbase.cloud:8888
