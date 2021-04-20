b33b2cb8b90643d3b40d4b909286a20c89cdf001
João Luzio <joao.luzio@gmail.com>
2021-04-20 18:35:19 +0100

    doc

--- etc/scripts/baas/template/setup-aws-kube.sh
+++ etc/scripts/baas/template/setup-aws-kube.sh
@@ -5,4 +5,4 @@ script_dir="$( dirname "${BASH_SOURCE[0]}" )"
 . $script_dir/setup-aws.sh $@
 . $script_dir/setup-kube.sh $@
 
-[[ "$2" == --gen || "$2" == -g ]] && { . $script_dir/setup-kubeconfig.sh $@; }
+[[ "$2" == --gen || "$2" == -g ]] && { . $script_dir/setup-app-kubeconfig.sh $@; }
