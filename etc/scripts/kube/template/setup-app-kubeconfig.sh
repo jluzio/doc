059c478bca2f429523d3e50df1f4fa17aa5d218d
João Luzio <joao.luzio@gmail.com>
2021-05-06 14:56:51 +0100

    doc

--- etc/scripts/kube/template/setup-app-kubeconfig.sh
+++ etc/scripts/kube/template/setup-app-kubeconfig.sh
@@ -6,4 +6,4 @@ mkdir -p $(dirname $KUBECONFIG)
 # example tkgi/pks
 #PASS="<pass>"
 #echo $PASS | tkgi login -a <tkgi_or_pks_api_server> -u <service_user> -k
-#echo $PASS | tkgi get-credentials ${1:dev}
+#echo $PASS | tkgi get-credentials ${1:-dev}
