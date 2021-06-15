2244c92b69bef73fe1dc01a54a3e1a45f587c94b
João Luzio <joao.luzio@gmail.com>
2021-06-15 20:47:59 +0100

    doc

--- etc/scripts/kube/template/setup-app-kubeconfig-copy.sh
+++ etc/scripts/kube/template/setup-app-kubeconfig-copy.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+source_cfg=<source_home>/app/config
 output_dir=<target_home>/.kube/app
 output_cfg=$output_dir/config
 
 # cp / scp / rsync / etc
-cp ~/.kube/app/config $output_cfg
+cp $source_cfg $output_cfg
 
 # useful for ssh tunnels, while mantaining the original host name
-sed -i 's/<host>/<host_alias>/' $output_cfg
+sed -i 's/<host>/<host_alias>/g' $output_cfg
